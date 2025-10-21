a19b7bc5b7c2fff24330e669fdde4be8147bd55e
Gabriela Lima <gabirielaestudos010209@gmail.com>
2025-10-21 12:50:57 -0300

    21/10

---
+++ main.py
@@ -0,0 +1,18 @@
+def Nome():
+    nome = input("Entre com o seu nome: ")
+    print("Bem vindo", nome)
+Nome()
+
+
+def Salario():
+    salario = float(input("Entre com salario do mes: "))
+    print("Seu salario é R$", salario)
+Salario()
+
+
+def Soma():
+    valor1= int(input("Entre com o valor 1: "))
+    valor2= int(input("Entre com o valor 2: "))
+    soma = valor1 + valor2
+    print(soma)
+Soma()
