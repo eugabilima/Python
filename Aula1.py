30f37bcdbc1a55f14d62c46517b7e8a260a2611b
Gabriela Lima <gabirielaestudos010209@gmail.com>
2025-10-23 11:16:56 -0300

    python

---
+++ Aula1.py
@@ -0,0 +1,35 @@
+print( "Olá, mundo!")
+
+
+nome = "Gabi"
+idade = 16
+print("meu nome é", nome, "e tenho", idade, "anos")
+
+#Entrada de Dados
+
+nome = input("Qual o seu nome? ")
+print("Bem-vindo(a)", nome,"!")
+
+#Estruturas condicionais simples
+
+nota= 7
+if nota>= 6:
+  print("Aprovado!")
+else:
+  print("Reprovado!")
+  
+
+v1 = input("Entre com o valor 1")
+v1 = int(v1)
+v2 = input("Entre com o valor 2")
+v2 = int(v2)
+     
+
+#Operações Matematicas
+
+a = 10
+b = 5
+print("Soma:", a + b)
+print("Subtração", a - b)
+print("Multiplicação", a * b)
+print("Divisão", a / b)
